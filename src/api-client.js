d3342728d5dbc285f7f88aabb50b61e29ff89392
Danis Idiatullin <danisidiatullin1993@gmail.com>
2022-12-08 13:33:22 +0100

    add axios using to the project

--- src/api-client.js
+++ src/api-client.js
@@ -1,158 +1,127 @@
-const requestOptions = ({
-  method,
-  content_type = "application/json",
-  token = null,
-  body = null,
-}) => {
-  let headers = { "Content-Type": content_type };
-  if (token !== null) {
-    headers["Authorization"] = `Bearer ${token}`;
-  }
-  if (method !== "GET" && method !== "DELETE") {
-    body = JSON.stringify(body);
-    return {
-      method: method,
-      headers: headers,
-      body: body,
-    };
-  }
-
-  return {
-    method: method,
-    headers: headers,
-  };
-};
+import API from "./api.js";
 
 export class ApiClient {
   static getTitle = async () => {
-    const response = await fetch("/api", requestOptions({ method: "GET" }));
-    const data = await response.json();
-
-    if (!response.ok) {
+    try {
+      const response = await API.get("/api");
+      return response.data.message;
+    } catch (error) {
       console.log("something messed up");
-    } else {
-      return data.message;
     }
   };
 
   static submitRegistration = async (body) => {
-    const response = await fetch(
-      "/api/users",
-      requestOptions({ method: "POST", body: body })
-    );
-    const data = await response.json();
-
-    if (!response.ok) {
-      return { error: data.detail, access_token: null };
+    try {
+      var response = await API.post("/api/users", body);
+      return { access_token: response.data.access_token, error: null };
+    } catch (error) {
+      return { error: response.data.detail, access_token: null };
     }
-    return { access_token: data.access_token, error: null };
   };
 
   static submitLogin = async (body) => {
-    const response = await fetch(
-      "/api/token",
-      requestOptions({
-        method: "POST",
-        content_type: "application/x-www-form-urlencoded",
-        body: `grant_type=&username=${body.email}&password=${body.password}&scope=&client_id=&client_secret=`,
-      })
-    );
-    const data = await response.json();
-
-    if (!response.ok) {
-      return { error: data.detail, access_token: null };
+    try {
+      var response = await API.post(
+        "/api/token",
+        `grant_type=&username=${body.email}&password=${body.password}&scope=&client_id=&client_secret=`,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      return { access_token: response.data.access_token, error: null };
+    } catch {
+      return { error: response.data.detail, access_token: null };
     }
-    return { access_token: data.access_token, error: null };
   };
 
   static fetchUser = async (token) => {
-    const response = await fetch(
-      "/api/users/me",
-      requestOptions({ method: "GET", token: token })
-    );
-    const data = await response.json();
-
-    if (!response.ok) {
+    try {
+      await API.get("/api/users/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return token;
+    } catch (error) {
       return null;
     }
-    return token;
   };
 
   static getTasks = async (token) => {
-    const response = await fetch(
-      "/api/tasks",
-      requestOptions({ method: "GET", token: token })
-    );
-    const data = await response.json();
-
-    if (!response.ok) {
-      return { error: true };
-    } else {
+    try {
+      const response = await API.get("/api/tasks", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.data;
       return { error: false, data: data };
+    } catch (error) {
+      return { error: true };
     }
   };
 
   static getTask = async (id, token) => {
-    const response = await fetch(
-      `/api/tasks/${id}`,
-      requestOptions({ method: "GET", token: token })
-    );
-    const data = await response.json();
-
-    if (!response.ok) {
-      return { error: true };
-    } else {
+    try {
+      const response = await API.get(`/api/tasks/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.data;
       return { error: false, data: data };
+    } catch (error) {
+      return { error: true };
     }
   };
 
   static createTask = async (token, body) => {
-    const response = await fetch(
-      "/api/tasks",
-      requestOptions({ method: "POST", token: token, body: body })
-    );
-    if (!response.ok) {
-      return false;
-    } else {
+    try {
+      await API.post("/api/tasks", body, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return true;
+    } catch (error) {
+      return false;
     }
   };
 
   static updateTask = async (id, token, body) => {
-    const response = await fetch(
-      `/api/tasks/${id}`,
-      requestOptions({ method: "PUT", token: token, body: body })
-    );
-    if (!response.ok) {
-      return false;
-    } else {
+    try {
+      await API.put(`/api/tasks/${id}`, body, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return true;
+    } catch (error) {
+      return false;
     }
   };
 
   static deleteTask = async (id, token) => {
-    const response = await fetch(
-      `/api/tasks/${id}`,
-      requestOptions({ method: "DELETE", token: token })
-    );
-    if (!response.ok) {
-      return false;
-    } else {
+    try {
+      await API.delete(`/api/tasks/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return true;
+    } catch (error) {
+      return false;
     }
   };
 
   static getStatuses = async () => {
-    const response = await fetch(
-      "/api/task_statuses",
-      requestOptions({ method: "GET" })
-    );
-    const data = await response.json();
-
-    if (!response.ok) {
+    try {
+      const response = await API.get("/api/task_statuses");
+      return { error: false, data: response.data };
+    } catch (error) {
       return { error: true };
-    } else {
-      return { error: false, data: data };
     }
   };
 }
